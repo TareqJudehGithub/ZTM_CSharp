5c7d30de1eebf1e468f285bce140d7f97246e9af
TareqJudehGithub <tareq.joudeh@gmail.com>
2023-12-12 20:03:30 +0300

    Completed Methods lecture and exercises.

---
+++ Fund/OOP/Methods/MethodsIntro/ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿public class Product
+{
+    // Fields
+    public int productID;
+    public string productName;
+    public double cost;
+    public int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Electronics";
+    public readonly string dateOfPurchase;
+
+    public double tax;
+
+    #region Constructors 
+    public Product()
+    {
+        dateOfPurchase = DateTime.Now.ToShortDateString();
+    }
+    #endregion
+
+    #region Methods
+    // Write a problem (method) to calculate tax
+    // Tax = 10% if the cost is less or equal than $20k, and it is 12.5% if the cost is > 20k
+
+    //public double CalculateTax()
+    //{
+    //    double taxAmount;
+
+    //    if (cost <= 500)
+    //    {
+    //        tax = 0.12;
+    //        taxAmount = cost * tax;
+    //    }
+    //    else
+    //    {
+    //        tax = 0.125;
+    //        taxAmount = cost * tax;
+    //    }
+    //    Console.WriteLine($"Tax percenage = {tax}");
+    //    return taxAmount;
+    //}
+    public void CalculateTax()
+    {
+        double taxAmount;
+
+        if (cost <= 500)
+        {
+            tax = 0.12;
+            taxAmount = cost * tax;
+        }
+        else
+        {
+            tax = 0.125;
+            taxAmount = cost * tax;
+        }
+        tax = taxAmount;
+    }
+    #endregion
+
+}
---
+++ Fund/OOP/Methods/MethodsIntro/MethodsIntro/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace MethodsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Methods\n");
+
+            // Objects
+            int prodsTotal = Product.TotalNoProducts;
+
+            Product productA = new Product();
+            prodsTotal++;
+            Product productB = new Product();
+            prodsTotal++;
+
+            // Product A
+            int prodAID = productA.productID = 1001;
+            string prodAName = productA.productName = "Electric Heater";
+            double prodACost = productA.cost = 235.245D;
+
+            productA.CalculateTax();
+            double prodAtaxAmount = productA.tax;
+            int prodAQuantity = productA.quantityInStock = 245;
+            string prodACat = Product.CategoryName;
+            string prodADate = productA.dateOfPurchase;
+
+            // Product B
+            int prodBID = productB.productID = 1002;
+            string prodBName = productB.productName = "LED TV";
+            double prodBCost = productB.cost = 587.345D;
+
+            productB.CalculateTax();
+            double prodBtaxAmount = productB.tax;
+            int prodBQuantity = productB.quantityInStock = 1257;
+            string prodBCat = Product.CategoryName;
+
+            string prodBDate = productB.dateOfPurchase;
+
+
+            // Outputs
+            Console.WriteLine("Product 1");
+            Console.WriteLine($"Product ID: {prodAID}");
+            Console.WriteLine($"Product Name: {prodAName}");
+            Console.WriteLine($"Product Cost: {prodACost:C2}");
+            Console.WriteLine($"Tax Amount: {prodAtaxAmount:C}");
+
+            Console.WriteLine($"Product Quantity in stock: {prodAQuantity}");
+            Console.WriteLine($"Product Category: {prodACat}");
+            Console.WriteLine($"Product Date of Purchase: {prodADate}");
+            Console.WriteLine($"Product ID: {prodAID}\n");
+
+            Console.WriteLine("Product 2");
+            Console.WriteLine($"Product ID: {prodBID}");
+            Console.WriteLine($"Product Name: {prodBName}");
+            Console.WriteLine($"Product Cost: {prodBCost:C2}");
+            Console.WriteLine($"Tax Amount: {prodBtaxAmount:C}");
+
+            Console.WriteLine($"Product Quantity in stock: {prodBQuantity}");
+            Console.WriteLine($"Product Category: {prodBCat}");
+            Console.WriteLine($"Product Date of Purchase: {prodBDate}\n");
+
+            Console.WriteLine($"Product Total: {prodsTotal++}");
+        }
+    }
+}
+
+
+/**
+ *   public int productID;
+    public string productName;
+    public double cost;
+    public int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Electronics";
+    public readonly string dateOfPurchase;
+ */
